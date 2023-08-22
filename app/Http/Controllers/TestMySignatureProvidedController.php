53ab9fe3abd1f9b161b04407b4e1609c45f557da
ElySarmiento <edrinesarmiento5@gmail.com>
2023-08-23 02:43:18 +0800

    version 0.8: just modify the testmysignature controller

--- app/Http/Controllers/TestMySignatureProvidedController.php
+++ app/Http/Controllers/TestMySignatureProvidedController.php
@@ -2,14 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Signature;
 use Illuminate\Http\Request;
 
 class TestMySignatureProvidedController extends Controller
 {
     //
+
+    public function getImages($userId){
+
+        $user = Signature::where('user_id',$userId)->first();
+
+        $userSignature = [
+            'signature_1' => $user->signature_1,
+            'signature_2' => $user->signature_2,
+            'signature_3' => $user->signature_3,
+        ];
+
+        return $userSignature;
+
+    }
+
+
     public function TestMySignature(Request $request){
 
-        
+        $SignatureImage = $this->getImages(auth()->id());
+
+        //ORB algorithm
 
     }
 }
